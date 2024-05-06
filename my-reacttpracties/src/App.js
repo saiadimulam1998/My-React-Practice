936155ace57979c3a8d398c8b702c89073b5d5ef
saiadimulam1998 <saiadimulam199805@gmail.com>
2024-05-06 03:26:28 -0700

    arey babu

--- my-reacttpracties/src/App.js
+++ my-reacttpracties/src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path='/HTML' element={<div><h2>HTML</h2><p>HELLO</p></div>} />
           <Route path='/CSS' element={<div><h2>CSS</h2><p>HELLO</p></div>} />
           <Route path='/Javascript' element={<div><h2>Javascript</h2><p>HELLO</p></div>} />
+          <Route path='/sai' element={<div><h2>hello git</h2><p>huuu</p></div>} />
           
         </Routes>
       </div>
