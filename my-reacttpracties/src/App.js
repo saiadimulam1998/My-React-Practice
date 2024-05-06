8dd6b2442ced2c904276b5e7358ebc0662d1de92
saiadimulam1998 <saiadimulam199805@gmail.com>
2024-05-06 03:16:25 -0700

    my second commit

--- my-reacttpracties/src/App.js
+++ my-reacttpracties/src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path='/HTML' element={<div><h2>HTML</h2><p>HELLO</p></div>} />
           <Route path='/CSS' element={<div><h2>CSS</h2><p>HELLO</p></div>} />
           <Route path='/Javascript' element={<div><h2>Javascript</h2><p>HELLO</p></div>} />
+          <Route path='/sai' element={<div><h2>Sai</h2><p>Sai</p></div>} />
         </Routes>
       </div>
     </BrowserRouter>
