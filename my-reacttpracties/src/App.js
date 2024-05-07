e6309973f94266302829d9530ad7351b171158c5
saiadimulam1998 <saiadimulam199805@gmail.com>
2024-05-07 01:09:22 -0700

    Router

--- my-reacttpracties/src/App.js
+++ my-reacttpracties/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -7,17 +7,20 @@ function App() {
       <div className='container-fluid'>
         <h2>Tutorial Home</h2>
         <ol>
-          <li>HTML</li>
-          <li>CSS</li>
+          <li> <Link to="/HTML">HTML</Link></li>
+          <li><Link to="/CSS">SAI</Link></li>
+          <li><Link to="/Arey Babu Liesen care fully">SAI</Link></li>
           <li>Javascript</li>
         </ol>
         <hr size="4" noshade/>
 
         <Routes>
-          <Route path='/HTML' element={<div><h2>HTML</h2><p>HELLO</p></div>} />
-          <Route path='/CSS' element={<div><h2>CSS</h2><p>HELLO</p></div>} />
+          <Route path='HTML' element={<div><h2>HTML</h2><p>HELLO</p></div>} />
+          <Route path='/CSS' element={<div><h2>CSS</h2><p>CSS</p></div>} />
           <Route path='/Javascript' element={<div><h2>Javascript</h2><p>HELLO</p></div>} />
-          <Route path='/sai' element={<div><h2>hello git</h2><p>huuu</p></div>} />
+          <Route path='/sai' element={<div><h2>Javascript</h2><p>HELLO</p></div>} />
+          <Route path='/Arey Babu Liesen care fully' element={<div>Sure</div>} />
+          <Route path='/' element={<div>React Router Dom </div>} />
           
         </Routes>
       </div>
